97079ab9a343d5a91826a6a57c3007513084e335
nursakinah24 <nursakinah2402@gmail.com>
2022-03-16 14:29:58 +0700

    Add project files.

---
+++ Create_Table_Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Table_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
